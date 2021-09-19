9b24d78ffb03e2a09aaeb7a0f552c86309ae3657
jralvarenga <jralvarenga161@gmail.com>
2021-09-19 17:27:35 -0600

    Added notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,48 @@
 import 'react-native-gesture-handler'
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProviderStaging from './src/services/ProviderStaging'
+import messaging from '@react-native-firebase/messaging'
 
 const App = () => {
+
+  const setupCloudMessaging = async() => {
+    await messaging().registerDeviceForRemoteMessages()
+
+    const authStatus = await messaging().requestPermission()
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL
+
+    if (!enabled) {
+      console.log("Don't have permition for notifications")
+    }
+  }
+
+  useEffect(() => {
+    setupCloudMessaging();
+
+    // On app open gets notification
+    const unsubscribe = messaging().onMessage(async(message) => {
+      const title = message.notification?.title ? message.notification?.title : '--No title--'
+      console.log(`On message: ${title}`)
+    });
+
+    // In background notification open app
+    messaging().onNotificationOpenedApp(async(message) => {
+      const title = message.notification?.title ? message.notification?.title : '--No title--'
+      console.log(`Notification open app: ${title}`)
+    })
+
+    // In sleep app receive notification
+    messaging().getInitialNotification().then((message) => {
+      if (message) {
+        const title = message.notification?.title ? message.notification?.title : '--No title--'
+        console.log(`Initial notification: ${title}`)
+      }
+    })
+
+    return unsubscribe
+  }, [])
 
   return (
     <ProviderStaging />
--- mobile/index.js
+++ mobile/index.js
@@ -1,5 +1,17 @@
-import {AppRegistry} from 'react-native'
+import React from 'react'
+import { AppRegistry } from 'react-native'
 import App from './App'
 import {name as appName} from './app.json'
+import messaging from '@react-native-firebase/messaging'
 
-AppRegistry.registerComponent(appName, () => App)
+messaging().setBackgroundMessageHandler(async(message) => {
+  const title = message.notification?.title ? message.notification?.title : '--No title--'
+
+  console.log(`New Passlog notification: ${title}`)
+})
+
+function HeadlessCheck({isHeadless}) {
+  return isHeadless ? null : <App />
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck)
