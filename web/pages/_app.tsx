03b50d21073b164ba8ef97cd53903c033e806ca4
jralvarenga <jralvarenga161@gmail.com>
2021-09-23 00:27:56 -0600

    Added material ui

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { EmotionCache } from '@emotion/utils'
+import { createEmotionCache } from '../src/services/createEmotionCache'
+import { CacheProvider } from '@emotion/react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { darkTheme } from '../src/services/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
+  return (
+    // @ts-ignore
+    <CacheProvider value={emotionCache}>
+
+      <CssBaseline />
+      <ThemeProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 export default MyApp
