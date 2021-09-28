70d2f283fd22fe598bee9916c5320b5187e67cf8
jralvarenga <jralvarenga161@gmail.com>
2021-09-27 23:10:26 -0600

    Removed autoscaling mobile text

--- mobile/index.js
+++ mobile/index.js
@@ -3,6 +3,7 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import {name as appName} from './app.json'
 import messaging from '@react-native-firebase/messaging'
+import { Text } from 'react-native'
 import './i18n.ts'
 
 messaging().setBackgroundMessageHandler(async(message) => {
@@ -14,5 +15,8 @@ messaging().setBackgroundMessageHandler(async(message) => {
 function HeadlessCheck({isHeadless}) {
   return isHeadless ? null : <App />
 }
+
+Text.defaultProps = Text.defaultProps || {}
+Text.defaultProps.allowFontScaling = false
 
 AppRegistry.registerComponent(appName, () => HeadlessCheck)
