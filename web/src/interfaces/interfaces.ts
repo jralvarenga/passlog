7af47ff65a00ce87e901366eead57f5bfbfb1f8c
jralvarenga <jralvarenga161@gmail.com>
2021-10-01 22:23:12 -0600

    Added interface

---
+++ web/src/interfaces/interfaces.ts
@@ -0,0 +1,58 @@
+export interface PasswordProps {
+  profileName: string
+  password: string
+  id: string
+  email: string
+  comments: string
+  user: string
+  date: string
+}
+
+export interface CardProps {
+  addedInfo: string
+  cardName: string
+  date: string
+  holder: string
+  id: string
+  number: string
+  type: string
+}
+
+export interface NoteProps {
+  id: string
+  title: string
+  body: string
+  date: string
+}
+
+export interface PasslogUserDataProps {
+  passwords?: PasswordProps[]
+  setPasswords?: Function
+  cards?: CardProps[]
+  setCards?: Function
+  renderPasslogDataHandler?: Function
+  settings?: SettingsProps
+  setSettings?: Function
+  //user?: FirebaseAuthTypes.User | null
+  setUser?: Function
+  userSettings?: UserSettingsProps | null
+  setUserSettings?: Function
+  dataLoading?: boolean
+  setDataLoading?: Function
+  notes?: NoteProps[]
+  setNotes?: Function
+}
+
+export interface SettingsProps {
+  useBiometrics?: boolean
+  usePin?: boolean
+  pinNumber?: string
+  firstTime?: boolean
+  askForAlwaysSync?: boolean
+}
+
+export interface UserSettingsProps {
+  name: string
+  uid: string
+  alwaysSync: boolean
+}
